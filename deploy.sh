fa7ad34cd9c0b7f651d5ec4ace03f868b85b2d06
celsovierra <seu-email@gmail.com>
2026-09-24 21:31:18 -0300

    fix: usar sed para remover aspas

--- deploy.sh
+++ deploy.sh
@@ -227,8 +227,8 @@ DESCRICAO="NEWMOD-${DNS}"
 DATA_HORA=$(date '+%d/%m/%Y %H:%M')
 BACKUP_FILE="/tmp/backup_traccar.tar.gz"
 
-  DB_USER=$(grep -oP "database.user>\K[^<]+" "$XML" | head -1 | tr -d "\x22\x27")
-  DB_PASS_ATUAL=$(grep -oP "database.password>\K[^<]+" "$XML" | head -1 | tr -d "\x22\x27")
+  DB_USER=$(grep -oP "database.user.>\K[^<]+" "$XML" | head -1 | sed "s/[\x27\x22]//g")
+  DB_PASS_ATUAL=$(grep -oP "database.password.>\K[^<]+" "$XML" | head -1 | sed "s/[\x27\x22]//g")
 DB_NAME=$(grep -oP "jdbc:mysql://[^/]+/\K[^?]+" /opt/traccar/conf/traccar.xml | head -1)
 
 mysql -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" -e "
@@ -280,8 +280,8 @@ fi
 echo ">> Garantindo senha padrao do MySQL..."
 XML="/opt/traccar/conf/traccar.xml"
 if [ -f "$XML" ]; then
-  DB_USER=$(grep -oP "database.user>\K[^<]+" "$XML" | head -1 | tr -d "\x22\x27")
-  DB_PASS_ATUAL=$(grep -oP "database.password>\K[^<]+" "$XML" | head -1 | tr -d "\x22\x27")
+  DB_USER=$(grep -oP "database.user.>\K[^<]+" "$XML" | head -1 | sed "s/[\x27\x22]//g")
+  DB_PASS_ATUAL=$(grep -oP "database.password.>\K[^<]+" "$XML" | head -1 | sed "s/[\x27\x22]//g")
   DB_PASS_NOVA="Traccar@2026#Sec"
   if [ "$DB_PASS_ATUAL" != "$DB_PASS_NOVA" ]; then
     sudo mysql -e "ALTER USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS_NOVA'; FLUSH PRIVILEGES;"
